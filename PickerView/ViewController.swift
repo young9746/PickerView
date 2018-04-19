a8401e322012e8a46e5b3330b7c9bf7aa03c0180
D7703_26 <d7703_26@mac.local>
2018-04-19 13:19:46 +0900

    PickerView

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet var myPickerView: UIPickerView!
+    @IBOutlet var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // ViewController와 delegate 연결
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // UIPickerViewDataSource method
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { return 2
     }
-
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0{
+            return 10
+        } else {
+            return 50
+        }
+    }
+    
+    // UIPickerViewDelegate method
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return "1st component \(row)"
+        } else {
+            return "2nd component \(row)"
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        myLabel.text = "comp = \(component) row = \(row)"
+    }
 }
-
